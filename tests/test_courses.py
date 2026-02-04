4370b85a04cfbeaa733a3ed075c751c0a47b0631
VadimDorozhkin <dorozhkin1991@gmail.com>
2026-02-04 09:56:07 +0300

    Фикстуры Pytest ДЗ 2.0

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -5,22 +5,6 @@ from playwright.sync_api import sync_playwright, expect, Page
 @pytest.mark.courses
 @pytest.mark.regression
 def test_empty_courses_list(chromium_page_with_state: Page):
-
-        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-        email_input = chromium_page_with_state.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user@gmail.com')
-
-        username_input = chromium_page_with_state.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = chromium_page_with_state.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill("password")
-
-        registration_button = chromium_page_with_state.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-
         chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
         text_Courses = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
